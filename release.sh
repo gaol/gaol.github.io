8d86796d5fd5cd66349f9f3cf7a9e17564ce7387
Lin Gao <aoingl@gmail.com>
2020-03-20 17:48:18 +0800

    temporary solution

---
+++ release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cwd="$(cd $(dirname $0) && pwd)"
+
+echo -e "working directory: $cwd"
+
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out master branch into public worktree directory"
+git worktree add -B master public origin/master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+
+./hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to master branch for static website update" && git push origin master
+
+echo "Done!"
+
+
