bdd7aa2ee1e20b6abf8766d7a0bf5a40d87de885
Lin Gao <aoingl@gmail.com>
2020-03-26 16:02:31 +0800

    switch  to https

--- hugo.sh
+++ hugo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v "$(pwd):/src" -v "$(pwd)/public:/output" aoingl/docker-images:hugo $@
+docker run --rm -v "$(pwd):/src" -v "$(pwd)/public:/output" aoingl/docker-images:hugo --source /src --destination /output $@
